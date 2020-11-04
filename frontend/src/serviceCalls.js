8b10549eddfce3873d6a9341d9e50480769eab25
Sarah Akbar <sarahakbar@Sarahs-MBP.fios-router.home>
2020-11-03 23:04:29 -0500

    CORS hotfix on client side

--- frontend/src/serviceCalls.js
+++ frontend/src/serviceCalls.js
@@ -5,9 +5,6 @@ export async function PostData(path, userData) { //String path, userDate in JSON
     let res = 
         await fetch(url+path, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
             body: JSON.stringify(userData)
        }).then((response) => response.json())
     
